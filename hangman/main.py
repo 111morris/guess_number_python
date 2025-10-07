6764e8112334a24525b84569c801a14010b7d322
mulandi <mulandimorris1@gmail.com>
2025-10-07 13:11:18 +0300

    update main file

--- hangman/main.py
+++ hangman/main.py
@@ -9,4 +9,22 @@ def get_valid_word(words):
         word = random.choice(words)
     return word 
 
+def hangman(): 
+    word = get_valid_word(words)
+    word_letters = set(word) 
+    alphabet = set(string.ascii_uppercase)
+    used_letter = set() 
 
+
+    # getting user input 
+    while len(word_letters) > 0:
+        user_letter = input("Guess a letter: ").upper()
+
+        if user_letter in alphabet - user_letters: 
+            used_letters.add(user_letter)
+            if user_letter in word_letters: 
+            word_letters.remove(user_letter)
+        elif user_letter in used_letters: 
+            print('You have already used that character. Please try again.')
+        else: 
+            print('You didn\'t type the correct word')
